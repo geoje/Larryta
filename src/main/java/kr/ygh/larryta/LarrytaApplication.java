3d1db7226fbf58af15ea47ebd1501b39c66590e5
Henry Yang <gyeongho.yang@deliveryhero.com>
2026-06-11 22:10:05 +0200

    init project

---
+++ src/main/java/kr/ygh/larryta/LarrytaApplication.java
@@ -0,0 +1,13 @@
+package kr.ygh.larryta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarrytaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LarrytaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/kr/ygh/larryta/LarrytaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ygh.larryta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarrytaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
